dd43bb1d7b2e2186ec65080dd70f48057846c2a1
tomlefoll <tom.lefoll44@gmail.com>
2024-12-19 16:17:51 +0100

    python

--- main.py
+++ main.py
@@ -1,1 +1,85 @@
-print("Five is greater than two!")
+if 5 > 2:
+    print("five is greater than two!")
+    if 5 > 2:
+            print("five is greater than two!")
+x = 5
+y = "Hello, World!" 
+
+#this is a comment.
+print(y)
+
+#this is a comment
+print("Hello, Worl")
+print("Hello, World")#this is a comment
+
+#print("Hello, World!")
+print("cheers, Mate!")
+
+#this is a comment
+#written in
+#more than juste one line
+print("Hello, World!")
+"""
+this is a comment
+witren in 
+more than just one line
+"""
+print ("Hello, World")
+
+x = 5
+y = "john"
+print(x)
+print(y)
+x = 4   #x is of type int 
+x = "sally"  # x is now of type str
+print(x)
+
+x = str(3)  # x will be '3'
+y = int(3)  # y will be 3
+z = float(3)  # z will be 3.0
+
+x = 5
+y = "john"
+print(type(x))
+print(type(y))
+x = "john"
+# is the same as 
+x = 'john'
+
+a = 4
+A = "Sally"
+#A will not overwite a 
+
+myvar = "john"
+my_var = "john"
+_my_var = "john"
+myVar = "john"
+MYVAR = "john"
+myvaré = "john"
+
+myVariableleName = "john"
+
+x, y, z, = "Orange","Banana","Cherry"
+print(x)
+print(y)
+print(z)
+
+x = y = z = "orange"
+print(x)
+print(y)
+print(z)
+
+fruits = ["apple", "banana","cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+
+x = "Python is awesom"
+print(x)
+
+x = "python"
+y = "is"
+z = "awesome"
+print(x, y, z)
