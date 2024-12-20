f0c790bdc4f7f76b705448ae398bd5b7e43ea2b7
tomlefoll <tom.lefoll44@gmail.com>
2024-12-20 09:49:34 +0100

    finish

--- main.py
+++ main.py
@@ -83,3 +83,59 @@ x = "python"
 y = "is"
 z = "awesome"
 print(x, y, z)
+
+x = " python"
+y = " is"
+z = " awzsome"
+print(x + y + z)
+
+x = 5
+y = 10
+print(x + y)
+
+x = 5
+y = "john"
+print(x, y)
+
+x = "awesome"
+def myfunc():
+    print("Python is " + x )
+
+    myfunc()
+
+    x = "awesome"
+
+    def myfunc():
+        x = "fantastic"
+        print("python is" + x)
+
+        myfunc()
+
+
+print("python is " + x )
+
+def myfunc():
+    global x 
+    x = "fantastic"
+    myfunc()
+
+    print("python is " + x) 
+
+    x = "awesome"
+    def myfunc():
+        x = "fantastic"
+        myfunc()
+        print("python is " + x)
+        
+
+
+
+
+        
+        
+
+
+
+
+   
+
